b842e3645f252ed43db6d4090d99c48af9779504
Laith Mihyar <LaithMihyar@Laiths-MacBook-Pro.local>
2017-02-27 12:55:57 -0600

    continue designing

---
+++ AnaVolunteer/RoundButton.swift
@@ -0,0 +1,16 @@
+//
+//  RoundButton.swift
+//  AnaVolunteer
+//
+//  Created by Laith Mihyar on 2/27/17.
+//  Copyright © 2017 Laith Mihyar. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton{
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+    }
+}
